acd3dcff6f3607675aaf45b9f554b5684d31916c
ThatTanna <tanna1987@gmail.com>
2024-03-20 10:04:06 +0800

    refactored

---
+++ index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Welcome to Project Destiny!', 'Ready to Explore?'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+};
+var typed = new Typed('.typing', options);
